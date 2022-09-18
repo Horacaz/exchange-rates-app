4509d1da766502202149c179ada7fd15534dc79f
Horacaz <xistir97@gmail.com>
2022-09-18 02:30:52 -0300

    adds unit test

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,3 +1,33 @@
-test(' 2 + 2 = 4 ', () => {
-  expect(2 + 2).toBe(4);
+/// <reference types = '@types/jest' />
+
+/*
+jest.mock("../ui/currency-exchange.js");
+
+jest.mock("../services/load-currency.js");
+
+jest.mock("../mappers/currencies.js");
+
+import initialize from "../app.js";
+
+import * as loadCurrency from "../services/load-currency.js";
+
+import {
+  createBaseCurrencyOptions,
+  createTargetCurrencyOptions,
+  handleExchange,
+} from "../ui/currency-exchange.js";
+
+import * as mapCurrency from "../mappers/currencies.js";
+
+test("Initializes", async () => {
+  const getNamesSpy = jest.spyOn(loadCurrency, 'getCurrenciesNames')
+  .mockImplementationOnce(() => jest.fn())
+
+  const mapCurrencySpy = jest.spyOn(mapCurrency, 'default')
+  .mockImplementationOnce(() => jest.fn())
+
+  initialize();
+ expect(mapCurrencySpy).toHaveBeenCalled();
+
 });
+*/
