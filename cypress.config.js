f7f5c63ca829812a87a82dea6732730ad05d2572
Horacaz <xistir97@gmail.com>
2023-01-19 06:24:00 +0900

    Fix Cypress config file adding e2e configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,14 @@
-// eslint-disable-next-line
+/* eslint-disable */
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   videos: {
     video: false,
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
